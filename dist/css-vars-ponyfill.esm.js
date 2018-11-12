3fdf7b4d71fe9bbb5b8be229fbf8a054a48c5b5a
John Hildenbiddle <john@hildenbiddle.com>
2018-11-11 18:25:47 -0800

    Update dependencies

--- dist/css-vars-ponyfill.esm.js
+++ dist/css-vars-ponyfill.esm.js
@@ -26,7 +26,7 @@ function _nonIterableSpread() {
 
 /*!
  * get-css-data
- * v1.4.0
+ * v1.5.0
  * https://github.com/jhildenbiddle/get-css-data
  * (c) 2018 John Hildenbiddle <http://hildenbiddle.com>
  * MIT license
@@ -43,6 +43,11 @@ function _nonIterableSpread() {
     var urlQueue = Array.apply(null, Array(urlArray.length)).map(function(x) {
         return null;
     });
+    function isValidCss() {
+        var cssText = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+        var isHTML = cssText.trim().charAt(0) === "<";
+        return !isHTML;
+    }
     function onError(xhr, urlIndex) {
         settings.onError(xhr, urlArray[urlIndex], urlIndex);
     }
@@ -68,7 +73,11 @@ function _nonIterableSpread() {
                 xdr.onprogress = Function.prototype;
                 xdr.ontimeout = Function.prototype;
                 xdr.onload = function() {
-                    onSuccess(xdr.responseText, i);
+                    if (isValidCss(xdr.responseText)) {
+                        onSuccess(xdr.responseText, i);
+                    } else {
+                        onError(xdr, i);
+                    }
                 };
                 xdr.onerror = function(err) {
                     onError(xdr, i);
@@ -89,7 +98,7 @@ function _nonIterableSpread() {
             settings.onBeforeSend(xhr, url, i);
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
+                    if (xhr.status === 200 && isValidCss(xhr.responseText)) {
                         onSuccess(xhr.responseText, i);
                     } else {
                         onError(xhr, i);
