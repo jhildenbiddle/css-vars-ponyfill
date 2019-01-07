6776ae0d0302de526028527a178186a0a837a0e3
John Hildenbiddle <john@hildenbiddle.com>
2019-01-06 18:59:06 -0800

    Fix Karma+Chrome launch on Travis

--- karma.conf.js
+++ karma.conf.js
@@ -210,7 +210,13 @@ module.exports = function(config) {
             customLaunchers: {
                 ChromeNoSandbox: {
                     base : 'Chrome',
-                    flags: ['--no-sandbox']
+                    flags: [
+                        '--disable-gpu',
+                        '--headless',
+                        '--no-sandbox',
+                        '--remote-debugging-address=0.0.0.0',
+                        '--remote-debugging-port=9222',
+                        '--user-data-dir=/tmp'
                 }
             }
         });
