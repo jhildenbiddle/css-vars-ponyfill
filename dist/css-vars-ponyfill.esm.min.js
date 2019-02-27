9e5e18ff9190a5b73303030fe70edd1aec5dbcb6
John Hildenbiddle <john@hildenbiddle.com>
2019-02-27 00:22:18 -0800

    1.17.0

--- dist/css-vars-ponyfill.esm.min.js
+++ dist/css-vars-ponyfill.esm.min.js
@@ -1,6 +1,6 @@
 /*!
  * css-vars-ponyfill
- * v1.16.4
+ * v1.17.0
  * https://github.com/jhildenbiddle/css-vars-ponyfill
  * (c) 2018-2019 John Hildenbiddle <http://hildenbiddle.com>
  * MIT license
