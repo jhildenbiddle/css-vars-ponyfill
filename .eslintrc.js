6229ba9e1192df7b25d8dc7f4f41266bb36832f3
John Hildenbiddle <jhildenbiddle@users.noreply.github.com>
2021-08-26 16:27:42 -0500

    Remove line break style check

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
         'dot-notation'               : ['error'],
         'eqeqeq'                     : ['error'],
         'indent'                     : ['error', 4, {'SwitchCase': 1}],
-        'linebreak-style'            : ['error', 'unix'],
         'no-console'                 : ['warn'],
         'no-floating-decimal'        : ['error'],
         'no-implicit-coercion'       : ['error'],
