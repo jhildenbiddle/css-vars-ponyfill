bd0eb9857883d5cb4c8a3febe36d8fd40265b59b
John Hildenbiddle <jhildenbiddle@users.noreply.github.com>
2021-08-26 12:03:46 -0500

    Fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,44 @@
 module.exports = {
-    "env": {
-        "browser" : true,
-        "commonjs": true,
-        "es6"     : true,
-        "node"    : true
+    'env': {
+        'browser' : true,
+        'commonjs': true,
+        'es6'     : true,
+        'node'    : true
     },
-    "extends": [
-        "eslint:recommended"
+    'extends': [
+        'eslint:recommended'
     ],
-    "parserOptions": {
-        "sourceType": "module"
+    'ignorePatterns': [
+        'dist'
+    ],
+    'parserOptions': {
+        'sourceType': 'module'
     },
-    "plugins": [],
-    "rules": {
-        "array-bracket-spacing"      : ["error", "never"],
-        "array-callback-return"      : ["error"],
-        "block-scoped-var"           : ["error"],
-        "block-spacing"              : ["error", "always"],
-        "curly"                      : ["error"],
-        "dot-notation"               : ["error"],
-        "eqeqeq"                     : ["error"],
-        "indent"                     : ["error", 4, {"SwitchCase": 1}],
-        "linebreak-style"            : ["error", "unix"],
-        "no-console"                 : ["warn"],
-        "no-floating-decimal"        : ["error"],
-        "no-implicit-coercion"       : ["error"],
-        "no-implicit-globals"        : ["error"],
-        "no-loop-func"               : ["error"],
-        "no-return-assign"           : ["error"],
-        "no-template-curly-in-string": ["error"],
-        "no-unneeded-ternary"        : ["error"],
-        "no-unused-vars"             : ["error", { "args": "none" }],
-        "no-useless-computed-key"    : ["error"],
-        "no-useless-return"          : ["error"],
-        "no-var"                     : ["error"],
-        "prefer-const"               : ["error"],
-        "quotes"                     : ["error", "single"],
-        "semi"                       : ["error", "always"]
+    'plugins': [],
+    'rules': {
+        'array-bracket-spacing'      : ['error', 'never'],
+        'array-callback-return'      : ['error'],
+        'block-scoped-var'           : ['error'],
+        'block-spacing'              : ['error', 'always'],
+        'curly'                      : ['error'],
+        'dot-notation'               : ['error'],
+        'eqeqeq'                     : ['error'],
+        'indent'                     : ['error', 4, {'SwitchCase': 1}],
+        'linebreak-style'            : ['error', 'unix'],
+        'no-console'                 : ['warn'],
+        'no-floating-decimal'        : ['error'],
+        'no-implicit-coercion'       : ['error'],
+        'no-implicit-globals'        : ['error'],
+        'no-loop-func'               : ['error'],
+        'no-return-assign'           : ['error'],
+        'no-template-curly-in-string': ['error'],
+        'no-unneeded-ternary'        : ['error'],
+        'no-unused-vars'             : ['error', { 'args': 'none' }],
+        'no-useless-computed-key'    : ['error'],
+        'no-useless-return'          : ['error'],
+        'no-var'                     : ['error'],
+        'prefer-const'               : ['error'],
+        'quotes'                     : ['error', 'single'],
+        'semi'                       : ['error', 'always']
     }
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,23 +1,23 @@
 // Cascading config (merges with parent config)
 // http://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
 module.exports = {
-    "env": {
-        "mocha": true
+    'env': {
+        'mocha': true
     },
-    "extends": [
-        "plugin:mocha/recommended"
+    'extends': [
+        'plugin:mocha/recommended'
     ],
-    "parserOptions": {
-        "ecmaVersion": 8,
+    'parserOptions': {
+        'ecmaVersion': 8,
     },
-    "plugins": [
-        "chai-expect",
-        "mocha"
+    'plugins': [
+        'chai-expect',
+        'mocha'
     ],
-    "rules": {
-        "mocha/no-hooks-for-single-case": ["off"],
-        "mocha/no-top-level-hooks"      : ["off"],
-        "mocha/no-setup-in-describe"    : ["off"],
-        "no-console"                    : ["off"]
+    'rules': {
+        'mocha/no-hooks-for-single-case': ['off'],
+        'mocha/no-top-level-hooks'      : ['off'],
+        'mocha/no-setup-in-describe'    : ['off'],
+        'no-console'                    : ['off']
     }
 };
